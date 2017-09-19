78b08e0a4b69fe606b1e212d9eabdf17827ae8a2
awallathome <adam.wallis@me.com>
2017-09-19 18:01:06 -0400

    Initial commit

---
+++ bamazonCustomer.js
@@ -0,0 +1,62 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connect = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon"
+});
+
+// connect to the mysql server and sql database
+connect.connect(function (err) {
+  if (err) throw err;
+  // run the start function after the connection is made to display products andprompt the user
+  console.log("Connected!");
+  start();
+});
+
+function start() {
+  connect.query("SELECT * FROM bamazon.products", function (err, res) {
+    if (err) throw err;
+    console.log("Here are some things for sale:" + "\n============================\n");
+    for (var i = 0; i < res.length; i++) {
+      console.log("item number: " + res[i].item_id + "\nProduct: " + res[i].product_name + "\nDepartment: " +
+        res[i].department_name + "\nPrice: " + res[i].price + "\nQuantity: " + res[i].stock_quantity + "\n")
+    };
+    inquirer.prompt({
+      name: "itemid",
+      type: "input",
+      message: "What is the ID of the item you want to buy?",
+      validate: function (value) {
+        var pass = value.match(/^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i);
+        if (pass) {
+          return true;
+        }
+        //COME BACK here to check for an integer. Maybe user parseint()? 
+        return true;
+      }
+    }).then(function (answers) {
+      inquirer.prompt({
+        name: "itemid",
+        type: "input",
+        message: "How many would you like to buy?",
+        validate: function (value) {
+          var total = connect.database.bamazon.products;
+          var pass = ((value > 0) && (value < total.length));
+          if (pass) {
+            return true;
+          }
+          //COME BACK here too to check for an integer AND to check if there are that many available...
+          return "We don't have that many.";
+        }
+      })
+    })
+  })
+}
